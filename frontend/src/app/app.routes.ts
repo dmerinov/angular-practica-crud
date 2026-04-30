0e17106a2714b04e73808e901b23f18073ba82f1
David M <davidmvecino@gmail.com>
2026-04-30 12:49:25 +0200

    added home page component. added to app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import HomePageComponent from './features/home-page-component/home-page-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home-page-component/home-page-component'),
+  },
+  {
+    path: '**', redirectTo: 'home',
+  }
+];
