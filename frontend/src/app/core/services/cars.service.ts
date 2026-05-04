e23b6472a86bc9fbeeae5091b1b1d9e869d6c252
David M <davidmvecino@gmail.com>
2026-05-04 12:32:09 +0200

    refactor service to use RxResource

--- frontend/src/app/core/services/cars.service.ts
+++ frontend/src/app/core/services/cars.service.ts
@@ -1,56 +1,75 @@
-import { inject, Injectable, signal } from '@angular/core';
-import { Car } from '../interfaces/car.interface';
-import {HttpClient, HttpParams } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { rxResource } from '@angular/core/rxjs-interop';
+
+import { catchError, map, of, throwError } from 'rxjs';
+
 import { CarListDTO } from '../interfaces/carsDTO.interfaces';
 import { CarMapper } from '../mappers/car.mapper';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root',
+})
 export class CarsService {
-  cars = signal<Car[]>([]);
-  currentPage = signal<number>(1)
-  totalPages = signal<number>(0)
-  loading = signal<boolean>(false)
   private httpClient = inject(HttpClient);
-  constructor() {
-    this.loadCars();
-  }
 
-  loadCars() {
-    if (this.loading()) return;
-    this.loading.set(true);
-    const params = new HttpParams({
-      fromObject: {
-        limit: 10,
-        page: this.currentPage()
-      }
-    });
-    this.httpClient.get<CarListDTO>('http://localhost:3000/cars',{
-      params: params
-    }).subscribe((response) => {
-      const newCars: Car[] = CarMapper.mapCarDTOArrayToCarArray(response.items);
-      console.log(response.items);
-      console.log(newCars);
-      this.cars.set(newCars);
-      this.totalPages.set(response.meta.totalPages);
-      this.loading.set(false);
-    });
-  }
+  currentPage = signal(1);
 
-  getNextPage(){
-    if(this.currentPage() < this.totalPages()){
-      console.log("currentPage ", this.currentPage());
-      console.log("totalPages ", this.totalPages());
-      this.currentPage.set(this.currentPage() + 1);
-      this.loadCars();
+  carsResource = rxResource({
+    params: () => ({
+      page: this.currentPage(),
+    }),
+
+    stream: ({ params }) => {
+      const httpParams = new HttpParams({
+        fromObject: {
+          limit: 10,
+          page: params.page,
+        },
+      });
+
+      return this.httpClient
+        .get<CarListDTO>('http://localhost:3000/cars', { params: httpParams })
+        .pipe(
+          map((response) => ({
+            cars: CarMapper.mapCarDTOArrayToCarArray(response.items),
+            totalPages: response.meta.totalPages,
+          })),
+
+          catchError((error) => {
+            console.error('Error cargando coches:', error);
+
+            return throwError(() => error);
+          }),
+        );
+    },
+  });
+
+  cars = computed(() => this.carsResource.value()?.cars ?? []);
+
+  totalPages = computed(() => this.carsResource.value()?.totalPages ?? 0);
+
+  loading = this.carsResource.isLoading;
+
+  error = computed(() => {
+    const err = this.carsResource.error();
+
+    if (!err) return null;
+
+    return 'No se pudieron cargar los coches';
+  });
+
+  getNextPage() {
+    if (this.currentPage() < this.totalPages()) {
+      this.currentPage.update((page) => page + 1);
     }
   }
 
-  getPreviousPage(){
+  getPreviousPage() {
     if (this.currentPage() > 1) {
-      console.log('currentPage ', this.currentPage());
-      console.log('totalPages ', this.totalPages());
-      this.currentPage.set(this.currentPage() - 1);
-      this.loadCars();
+      this.currentPage.update((page) => page - 1);
     }
   }
 }
